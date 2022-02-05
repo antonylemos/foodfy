2728bfba08375ca059cc4dc9f5c912837ab2448d
Rafael Ribeiro <rafaelnribeiro@outlook.com>
2022-02-05 03:36:00 -0300

    Separação do front e back do controlador de recipes

--- api/src/app/controllers/RecipeController.js
+++ api/src/app/controllers/RecipeController.js
@@ -16,16 +16,8 @@ module.exports = {
             recipes.length == 0
             ? pagination.total = 1
             : pagination.total = Math.ceil(recipes[0].total / params.limit);
-
-            const { success } = req.session;
-
-            if (success) {
-                res.render('admin/recipes/index', { recipes, pagination, success });
-                req.session.success = '';
-                return
-            }
             
-            return res.render('admin/recipes/index', { recipes, pagination });
+            return res.json({ recipes, pagination});
         } catch (err) {
             console.error(err);
         }
@@ -33,7 +25,7 @@ module.exports = {
     async userRecipes(req, res) {
         try {
             const params = getParams(req.query, 6);
-            params.id = req.session.userId;
+            //params.id = req.session.userId;
             
             const recipes = await loadRecipeService.load('recipes', params);
             const pagination = { page: params.page };
@@ -42,34 +34,27 @@ module.exports = {
             ? pagination.total = 1
             : pagination.total = Math.ceil(recipes[0].total / params.limit);
             
-            return res.render('admin/recipes/index', { recipes, pagination });
-        } catch (err) {
-            console.error(err);
-        }
-    },
-    async create(req, res) {
-        try {
-            const chefsOptions = await Recipe.chefsSelectOptions();
-            return res.render('admin/recipes/create', { chefsOptions });
+            return res.json({ recipes, pagination });
         } catch (err) {
             console.error(err);
         }
     },
     async post(req, res) {
         try {
-            const { chef: chef_id, title, ingredients,
+            const { chef_id, title, ingredients,
                 preparation, information } = req.body;
 
             const recipe_id = await Recipe.create({
                 chef_id,
-                user_id: req.session.userId,
+                //user_id: req.session.userId,
+                user_id: 3,
                 title,
                 ingredients,
                 preparation,
                 information
             });
 
-            const filesPromise = req.files.map(async file => {
+            const filesPromise = req.body.files.map(async file => {
                 const file_id = await File.create({
                     name: file.filename,
                     path: file.path
@@ -82,7 +67,7 @@ module.exports = {
 
             await Promise.all(filesPromise);
 
-            return res.redirect(`/admin/recipes/${recipe_id}`);
+            return res.json({recipe_id});
         } catch (err) {
             console.error(err);
         }
@@ -90,29 +75,19 @@ module.exports = {
     async show(req, res) {
         try {
             const recipe = await loadRecipeService.load('recipe', req.params.id);
-            if (!recipe) return res.send('Receita não encontrada!');
-            return res.render('admin/recipes/show', { recipe });
-        } catch (err) {
-            console.error(err);
-        }
-    },
-    async edit(req, res) {
-        try {
-            const recipe = await loadRecipeService.load('recipe', req.params.id);
-            if (!recipe) return res.send('Receita não encontrada!');
-            const chefsOptions = await Recipe.chefsSelectOptions();
-            return res.render('admin/recipes/edit', { recipe, chefsOptions });
+            if (!recipe) return res.json({error: 'Receita não encontrada!'});
+            return res.json({recipe});
         } catch (err) {
             console.error(err);
         }
     },
     async put(req, res) {
         try {
-            let { id, removed_files, chef: chef_id, title, ingredients,
+            let { id, removed_files, chef_id, title, ingredients,
                 preparation, information } = req.body;
 
-            if (req.files.length != 0) {
-                const newFilesPromise = req.files.map(async file => {
+            if (req.body.files.length != 0) {
+                const newFilesPromise = req.body.files.map(async file => {
                     const file_id = await File.create({
                         name: file.filename,
                         path: file.path
@@ -152,7 +127,7 @@ module.exports = {
                 information
             });
 
-            return res.redirect(`/admin/recipes/${id}`);
+            return res.json({id});
         } catch (err) {
             console.error(err);
         }
@@ -169,9 +144,9 @@ module.exports = {
 
             await Promise.all(deletedFilesPromise);
             await Recipe.delete({ id: req.body.id });
-            req.session.success = 'Receita excluída com sucesso!';
+            //req.session.success = 'Receita excluída com sucesso!';
 
-            return res.redirect('/admin');
+            return res.json();
         } catch (err) {
             console.error(err);
         }
--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -8,13 +8,19 @@ const { onlyUsers, isCreator } = require('../app/middlewares/sessions');
 const Validator = require('../app/validators/recipe');
 
 // Recipes Admin //
-routes.get('/', onlyUsers, RecipeController.index);
-routes.get('/my-recipes', onlyUsers, RecipeController.userRecipes);
-routes.get('/create', onlyUsers, RecipeController.create);
-routes.get('/:id', onlyUsers, RecipeController.show);
-routes.get('/:id/edit', onlyUsers, isCreator, RecipeController.edit);
-routes.post('/', onlyUsers, multer.array('photos', 5), Validator.post, RecipeController.post);
-routes.put('/', onlyUsers, multer.array('photos', 5),Validator.put, RecipeController.put);
-routes.delete('/', onlyUsers, RecipeController.delete);
+//routes.get('/', onlyUsers, RecipeController.index);
+routes.get('/', RecipeController.index);
+//routes.get('/my-recipes', onlyUsers, RecipeController.userRecipes);
+routes.get('/my-recipes', RecipeController.userRecipes);
+//routes.get('/create', onlyUsers, RecipeController.create);
+//routes.get('/:id', onlyUsers, RecipeController.show);
+routes.get('/:id', RecipeController.show);
+//routes.get('/:id/edit', onlyUsers, isCreator, RecipeController.edit);
+//routes.post('/', onlyUsers, multer.array('photos', 5), Validator.post, RecipeController.post);
+routes.post('/', RecipeController.post);
+//routes.put('/', onlyUsers, multer.array('photos', 5),Validator.put, RecipeController.put);
+routes.put('/', RecipeController.put);
+//routes.delete('/', onlyUsers, RecipeController.delete);
+routes.delete('/', RecipeController.delete);
 
 module.exports = routes;
